257ccecc9ef5ba2c939c27d2fee9f130f50e6a44
GabrielMMelo <gabrielmelocomp@gmail.com>
2018-06-14 19:56:49 -0300

    Working at second spinner on Sistema's fragment

--- app/src/main/java/gabrielmmelo/com/saaes/DadosSistemaFragment.java
+++ app/src/main/java/gabrielmmelo/com/saaes/DadosSistemaFragment.java
@@ -3,6 +3,7 @@ package gabrielmmelo.com.saaes;
 
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,16 +17,32 @@ import android.widget.Spinner;
  */
 public class DadosSistemaFragment extends Fragment {
 
+    private Spinner tipo_partida;
+    private Spinner banco_capacitores;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_dados_sistema, container, false);
-        Spinner dropdown = view.findViewById(R.id.spinner_tipo_partida);
-        String[] items = new String[]{"Partida Compensadora", "Estrela-triângulo", "Inversor de Frequência", "Chave Compensadora", "Soft-start", "Partida Direta"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_dropdown_item, items);
-        dropdown.setAdapter(adapter);
+
+        tipo_partida = view.findViewById(R.id.spinner_tipo_partida);
+        String[] items_tipo_partida = new String[]{"Partida Compensadora", "Estrela-triângulo", "Inversor de Frequência", "Chave Compensadora", "Soft-start", "Partida Direta"};
+        ArrayAdapter<String> adapter_tipo_partida = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_dropdown_item, items_tipo_partida);
+        tipo_partida.setAdapter(adapter_tipo_partida);
+
+        banco_capacitores = view.findViewById(R.id.spinner_banco_capacitores);
+        String[] items_banco_capactiores = new String[]{"Instalado na placa", "Não instalado"};
+        ArrayAdapter<String> adapter_banco_capacitores = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_dropdown_item, items_banco_capactiores);
+        banco_capacitores.setAdapter(adapter_banco_capacitores);
         return view;
     }
 
+    /*
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("tipo_partida", tipo_partida.getSelectedItem().toString());
+        outState.putString("banco_capacitores", tipo_partida.getSelectedItem().toString());
+    }
+    */
 }
