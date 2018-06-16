280afd9cd87f3a2b5ddf63c73cc614922199dab3
GabrielMMelo <gabrielmelocomp@gmail.com>
2018-06-16 11:38:46 -0300

    Clean and comment code

--- app/src/main/java/gabrielmmelo/com/saaes/DadosPlacaFragment.java
+++ app/src/main/java/gabrielmmelo/com/saaes/DadosPlacaFragment.java
@@ -17,24 +17,17 @@ import org.json.JSONObject;
 
 public class DadosPlacaFragment extends Fragment {
 
-    public DadosMotorBomba dadosPlaca = new DadosMotorBomba();
     public Context context;
+    private DadosMotorBomba dadosPlaca = new DadosMotorBomba();
     private ActivityCommunicator activityCommunicator;
 
     /*
-        CONSTRUCTOR
-     */
+        Constructor needed according to Android documentation
+    */
     public DadosPlacaFragment(){}
 
     public static DadosPlacaFragment newInstance(){
         return new DadosPlacaFragment();
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        this.context = getActivity();
-        activityCommunicator = (ActivityCommunicator)this.context;
     }
 
     /*
@@ -44,64 +37,29 @@ public class DadosPlacaFragment extends Fragment {
         public void passDadosPlacaToActivity(JSONObject json);
     }
 
+    /*
+        Get context to send data when fragment is attached
+    */
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.context = getActivity();
+        activityCommunicator = (ActivityCommunicator)this.context;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_dados_placa, container, false);
-        /*if(savedInstanceState != null){
-            Log.i("TESTE", "savedInstanceState is NOT null");
-            this.dadosPlaca.setTensao(savedInstanceState.getFloat("tensao"));
-            TextView tensao = (TextView) view.findViewById(R.id.tensao);
-            tensao.setText(Float.toString(this.dadosPlaca.getTensao()));
-
-            this.dadosPlaca.setCorrente(savedInstanceState.getFloat("corrente"));
-            TextView corrente = (TextView) view.findViewById(R.id.corrente);
-            corrente.setText(Float.toString(this.dadosPlaca.getCorrente()));
-
-            this.dadosPlaca.setPotencia_ativa(savedInstanceState.getFloat("potencia_ativa"));
-            TextView potencia_ativa = (TextView) view.findViewById(R.id.potencia_ativa);
-            potencia_ativa.setText(Float.toString(this.dadosPlaca.getPotencia_ativa()));
-
-            this.dadosPlaca.setPotencia_reativa(savedInstanceState.getFloat("potencia_reativa"));
-            TextView potencia_reativa = (TextView) view.findViewById(R.id.potencia_reativa);
-            potencia_reativa.setText(Float.toString(this.dadosPlaca.getPotencia_reativa()));
-
-            this.dadosPlaca.setFator_potencia(savedInstanceState.getFloat("fator_potencia"));
-            TextView fator_potencia = (TextView) view.findViewById(R.id.fator_potencia);
-            fator_potencia.setText(Float.toString(this.dadosPlaca.getFator_potencia()));
-
-            this.dadosPlaca.setRotacao(savedInstanceState.getFloat("rotacao"));
-            TextView rotacao = (TextView) view.findViewById(R.id.rotacao);
-            rotacao.setText(Float.toString(this.dadosPlaca.getRotacao()));
-
-            this.dadosPlaca.setFabricante_motor(savedInstanceState.getString("fabricante_motor"));
-            TextView fabricante_motor = (TextView) view.findViewById(R.id.fabricante_motor);
-            fabricante_motor.setText(this.dadosPlaca.getFabricante_motor());
-
-            this.dadosPlaca.setVazao(savedInstanceState.getFloat("vazao"));
-            TextView vazao = (TextView) view.findViewById(R.id.vazao);
-            vazao.setText(Float.toString(this.dadosPlaca.getVazao()));
-
-            this.dadosPlaca.setAltura_monometrica(savedInstanceState.getFloat("altura_monometrica"));
-            TextView altura_monometrica = (TextView) view.findViewById(R.id.altura_monometrica);
-            altura_monometrica.setText(Float.toString(this.dadosPlaca.getAltura_monometrica()));
-
-            this.dadosPlaca.setFabricante_bomba(savedInstanceState.getString("fabricante_bomba"));
-            TextView fabricante_bomba = (TextView) view.findViewById(R.id.fabricante_bomba);
-            fabricante_bomba.setText(this.dadosPlaca.getFabricante_bomba());
-        }
-
-        else{
-            Log.i("TESTE", "savedInstanceState is null");
-        }
-        */
 
         getActivity().setTitle("PLACA");
-        //setRetainInstance(true);
         return view;
     }
 
+    /*
+        Serialize and send data to Activity when the fragment is switched
+    */
     @Override
     public void onPause() {
         super.onPause();
@@ -122,11 +80,10 @@ public class DadosPlacaFragment extends Fragment {
         } catch (JSONException json_exception){
 
         }
-
     }
 
     /*
-        IT WONT BE CALLED BECAUSE onSaveInstaceState is only called when ACTIVITY it calls onSaveInstanceState (when the OS needs to destroy activity to clean space)
+        It wont be called because onSaveInstaceState is only called when ACTIVITY it calls onSaveInstanceState (when the OS needs to destroy activity to clean space)
     */
     @Override
     public void onSaveInstanceState(@NonNull Bundle outState) {
