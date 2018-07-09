653e73c3c76a0239a3a887d02dc47c2c7f49d005
GabrielMMelo <gabrielmelocomp@gmail.com>
2018-07-09 00:26:24 -0300

    Fix some strings

--- app/src/main/java/gabrielmmelo/com/saaes/DadosSistemaFragment.java
+++ app/src/main/java/gabrielmmelo/com/saaes/DadosSistemaFragment.java
@@ -19,6 +19,7 @@ public class DadosSistemaFragment extends Fragment {
 
     private Spinner tipo_partida;
     private Spinner banco_capacitores;
+    private Spinner sistema_supervisionado;
 
     /**
      * Constructor needed according to Android documentation
@@ -41,14 +42,19 @@ public class DadosSistemaFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_dados_sistema, container, false);
 
         tipo_partida = view.findViewById(R.id.spinner_tipo_partida);
-        String[] items_tipo_partida = new String[]{"Partida Compensadora", "Estrela-triângulo", "Inversor de Frequência", "Chave Compensadora", "Soft-start", "Partida Direta"};
+        String[] items_tipo_partida = new String[]{"Estrela-triângulo", "Inversor de Frequência", "Chave Compensadora", "Soft-start", "Partida Direta"};
         ArrayAdapter<String> adapter_tipo_partida = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_dropdown_item, items_tipo_partida);
         tipo_partida.setAdapter(adapter_tipo_partida);
 
         banco_capacitores = view.findViewById(R.id.spinner_banco_capacitores);
-        String[] items_banco_capactiores = new String[]{"Instalado na placa", "Não instalado"};
+        String[] items_banco_capactiores = new String[]{"Instalado no painel", "Não instalado"};
         ArrayAdapter<String> adapter_banco_capacitores = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_dropdown_item, items_banco_capactiores);
         banco_capacitores.setAdapter(adapter_banco_capacitores);
+
+        sistema_supervisionado = view.findViewById(R.id.spinner_sistema_supervisionado);
+        String[] items_sistema_supervisionado = new String[]{"Controlado", "Não controlado"};
+        ArrayAdapter<String> adapter_sistema_supervisionado = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_dropdown_item, items_sistema_supervisionado);
+        banco_capacitores.setAdapter(adapter_sistema_supervisionado);
 
         getActivity().setTitle("SISTEMA");
         return view;
